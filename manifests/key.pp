338280b1f46748a49a0bef5f97ccd5cc0b967bc4
Andreas Rammhold <andreas@rammhold.de>
2013-08-15 12:17:26 +0200

    Allow specifying of individual key file owners.
    This is useful for ceph-clients (using the ceph/rbd utility to interact with storage without being
    part of the cluster).
    
    Requires the ceph.conf file to be readable by the individual user.

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,12 +1,22 @@
 define ceph::key (
   $secret       = undef,
   $keyring_path = "/var/lib/ceph/tmp/${name}.keyring",
+  $user = 'root',
+  $group = 'root',
+  $mode = '0660',
 ) {
 
   exec { "ceph-key-${name}":
     command => "ceph-authtool ${keyring_path} --create-keyring --name='client.${name}' --add-key='${secret}'",
     creates => $keyring_path,
     require => Package['ceph'],
+    notify => File[$keyring_path]
   }
-
+  file { $keyring_path:
+    mode => $mode,
+    owner => $user,
+    group => $group,
+    require => Exec["ceph-key-${name}"]
+  }
+  Exec["ceph-key-${name}"] -> File["$keyring_path"]
 }
